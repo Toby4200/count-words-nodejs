1c355b34b2cccfaf92dab4f49c403656ead698a8
Бабин Анатолий Николаевич <ababin2@alfabank.ru>
2018-09-15 14:34:41 +0300

    feat: analyze bunch of file

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const fs = require('fs');
+const recursive = require("recursive-readdir");
 // count different words
 // outside of comment
 // in each file recursively on file tree
 // 
+let objToSaveWords = {};
 
-// count different words
+// Count different words
+recursive('./sources-to-count', function (err, files) {
+  // `files` is an array of file paths
+  console.log(files);
+  files.forEach((name, i, arr) => {
+        // get file
+        const file = fs.readFileSync(name, { encoding: 'utf-8'});
+        // console.log('file', file);
 
+        const wordsObj = countWords(file);
+        debugger;
+        console.log('wordsObj', wordsObj);
+        objToSaveWords = sumObjectsByKey(objToSaveWords, wordsObj);
+  });
 
-// get file
-const file = fs.readFileSync('./sources-to-count/once.js', { encoding: 'utf-8'});
-// console.log('file', file);
+  console.log('objToSaveWords', objToSaveWords);
+});
 
-const wordsObj = countWords(file);
-console.log('wordsObj', wordsObj);
 
 function countWords(string) {
 
@@ -29,17 +40,17 @@ function countWords(string) {
     /* The Array.prototype.reduce method assists us in producing a single value from an
        array. In this case, we're going to use it to output an object with results. */
     var counts = matchedWords.reduce(function ( stats, word ) {
-
+        console.log('stats', stats);
         /* `stats` is the object that we'll be building up over time.
            `word` is each individual entry in the `matchedWords` array */
-        if ( stats.hasOwnProperty( word ) ) {
+        if ( Object.prototype.hasOwnProperty.call(stats, word ) ) {
             /* `stats` already has an entry for the current `word`.
                As a result, let's increment the count for that `word`. */
-            stats[ word ] = stats[ word ] + 1;
+            stats[ word + '_' ] = stats[ word ] + 1;
         } else {
             /* `stats` does not yet have an entry for the current `word`.
                As a result, let's add a new entry, and set count to 1. */
-            stats[ word ] = 1;
+            stats[ word + '_' ] = 1;
         }
 
         /* Because we are building up `stats` over numerous iterations,
@@ -51,6 +62,15 @@ function countWords(string) {
     /* Now that `counts` has our object, we can log it. */
     // console.log( counts );
     return counts;
-
 };
+
+function sumObjectsByKey(...objs) {
+  return objs.reduce((a, b) => {
+    for (let k in b) {
+      if (Object.prototype.hasOwnProperty.call(b, k))
+        a[k] = (a[k] || 0) + b[k];
+    }
+    return a;
+  }, {});
+}
 
